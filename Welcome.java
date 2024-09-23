c00badc37bdc040ddb96baceaf49d71e321f2a95
Chobird <whdnjsql25@gmail.com>
2024-09-23 10:08:12 +0900

    Update and rename Welcom.java to Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,4 +1,4 @@
-public class Welcom {
+public class Welcome {
     public static void main(String[] args) {
         System.out.println("자바는 처음이죠?");
         System.out.println("Welcome to Java World");
